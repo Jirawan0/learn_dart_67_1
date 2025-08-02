4fc60ed53637e2bfe48924d3d0f00a80f3eb5571
Jirawan Kamolthipsukon <jirawan@example.com>
2025-08-02 15:21:07 +0700

    New update

---
+++ dart14.dart
@@ -0,0 +1,21 @@
+// set
+
+void main() {
+  Set<String> friuts = {'apple', 'banana', 'orange'};
+  print(friuts);
+
+  friuts.add('grape');
+  friuts.add('kiwi');
+  print(friuts);
+
+  friuts.remove('banana');
+  print(friuts);
+
+  Set<int> numbers = {1, 2, 3, 4, 5};
+  for (var number in numbers) {
+    print(number);
+  }
+
+  Set<String> cities = {'New York', 'Los Angeles', 'Chicago'};
+  print(cities.contains('Los Angeles')); //true
+}
