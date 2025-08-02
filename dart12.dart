125d30575b2aa41bd957680ea39e4c1ebfec3ef3
Jirawan Kamolthipsukon <jirawan@example.com>
2025-08-02 15:03:33 +0700

    New update

---
+++ dart12.dart
@@ -0,0 +1,22 @@
+// List
+
+void main() {
+  var ListVariable = new List<String>.filled(3, "", growable: false);
+  ListVariable[0] = "Apple";
+  ListVariable[1] = "Banana";
+  ListVariable[2] = "Cherry";
+  print(ListVariable);
+
+  var ListVarieble2 = [];
+  ListVarieble2 = ["Apple", "Banana", "Cherry"];
+  print(ListVarieble2);
+
+  var ListVariable3 = [1, 2, 3];
+  ListVariable3.add(4);
+  ListVariable3.add(5);
+  print(ListVariable3);
+
+  List<String> listVariable4 = ["Apple", "Banana", "Cherry"];
+  print(listVariable4[1]);
+  print(listVariable4);
+}
--- dart3.dart
+++ dart3.dart
@@ -1,18 +1,10 @@
 // Static Variable Type
-<<<<<<< HEAD
 void main() {
   // Sensitive case
-=======
-import 'dart:ffi';
-
-Void main() {
-  // Sensitive Case
->>>>>>> 19e3671c3823f7760226c63b866c41b27377b94e
   String name = "Alice";
   String _name = "Bob";
   String nAme = "Charlie";
 
-<<<<<<< HEAD
   print(name); // Alice
   print(_name); // Bob
   print(nAme); // Charlie
@@ -32,23 +24,3 @@ Void main() {
   print("น้ำหนัก: $Weight");
   print("นักเรียน: $isstudent");
 }
-=======
-  print(name);
-  print(_name);
-  print(nAme);
-  print("-------------------");
-
-  name = "Dayid";
-  int age = 20;
-  double score = 95.5;
-  num height = 175.5, weight = 70;
-  bool isStudent = true;
-
-  print("ชื่อ: $name");
-  print("อายุ: $age ปี");
-  print("คะแนน: $score");
-  print("ส่วนสูง: $Sheight ซม.");
-  print("น้ำหนัก: $weight กก.");
-  print("นักเรียน: $isStudent");
-}
->>>>>>> 19e3671c3823f7760226c63b866c41b27377b94e
